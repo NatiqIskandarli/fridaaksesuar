e982d6fc4accdc749ea7e35274fddbe35aadff90
NatiqIskandarli <natiq.elect@gmail.com>
2023-11-20 12:47:07 +0400

    payment test

---
+++ src/app/url/declinep/page.js
@@ -0,0 +1,40 @@
+'use client';
+import { useRouter } from 'next/navigation'
+import { useSelector,useDispatch } from "react-redux";
+import Section from "@/components/elements/Section";
+import FooterTwo from "@/components/footer/FooterTwo";
+import HeaderFive from "@/components/header/HeaderFive";
+import HeaderTwo from "@/components/header/HeaderTwo";
+import { checkOutRegister, checkOutSade } from '@/http/auth';
+import { useState,useEffect } from 'react';
+
+const DeclinePayment = () => {
+    const router = useRouter();
+    const [userIdd, setUserIdd] = useState('')
+    const dispatch = useDispatch();
+    const orders = useSelector((state) => state.productData.orderItems);
+    const latestOrder = orders[orders.length - 1];
+
+    useEffect(()=>{
+        const userId = localStorage.getItem("userid")
+        setUserIdd(userId)
+    },[])
+
+    
+
+    return ( 
+        <>
+        <HeaderTwo />
+            <main className="main-wrapper">
+                <Section pClass="order-received">                                
+                        <div className="order-details">                            
+                        decline
+                        </div>                      
+                </Section>
+            </main>
+        <FooterTwo />
+        </>
+    );
+}
+ 
+export default DeclinePayment;
