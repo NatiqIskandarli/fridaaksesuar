cbd338103bbeabee152d712ac947983fee747437
NatiqIskandarli <natiq.elect@gmail.com>
2023-11-21 10:35:02 +0400

    pause et

--- src/app/home/fashion/page.js
+++ src/app/home/fashion/page.js
@@ -40,74 +40,78 @@ const HomeFashion = () => {
     const exploreProduct = mapInSlices(fashionProduct, 4);
 
 
-    
-
-    return ( 
+    return (
         <>
-        <HeaderTwo />
-        <main className="main-wrapper">
-          {/* slide banner */}
-            <BannerThree />            
-            <CategoryFurniture />
-            {/* <ServiceOne /> */}            
-            {/* <ServiceTwo /> */}
-            <PosterTwo column="mb--10"/>
-            <Section>
-                <SectionTitle
-                title="Ən çox satılanlar"
-                subtitle=""
-                subtitleIcon=""
-                subColor="highlighter-primary"
-                />
-                <SlickSlider
-                class="explore-product-activation slick-layout-wrapper slick-layout-wrapper--15 axil-slick-arrow arrow-top-slide"
-                slidesToShow={1}
-                >
-                     {exploreProduct.slice(0, 2).map((product, index) => (
-                        <div key={index}>
-                            <div className="row row--15">
-                            {product.map((data) => (
-                                <div className="col-xl-3 col-lg-4 col-sm-6 col-12 mb--30" key={data.id}>
-                                    <ProductOne product={data} />
-                                </div>
-                            ))}
-                        </div>
-                        </div>
-                    ))}
-                </SlickSlider>        
-            </Section>
-            
-            
-            <Section>
-                <SectionTitle
-                title="Endirimdə olanlar"
-                subtitle=""
-                subtitleIcon=""
-                subColor="highlighter-primary"
-                />
-                <SlickSlider
-                class="explore-product-activation slick-layout-wrapper slick-layout-wrapper--15 axil-slick-arrow arrow-top-slide"
-                slidesToShow={1}
-                >
-                     {exploreProduct.slice(0, 2).map((product, index) => (
-                        <div key={index}>
-                            <div className="row row--15">
-                            {product.map((data) => (
-                                <div className="col-xl-3 col-lg-4 col-sm-6 col-12 mb--30" key={data.id}>
-                                    <ProductOne product={data} />
-                                </div>
-                            ))}
-                        </div>
-                        </div>
-                    ))}
-                </SlickSlider>        
-            </Section>           
-            
-            
-        </main>
-        <FooterTwo />
+        <h3>Texniki işlər aparılır</h3>
         </>
-    );
+    )
+
+    // return ( 
+    //     <>
+    //     <HeaderTwo />
+    //     <main className="main-wrapper">
+    //       {/* slide banner */}
+    //         <BannerThree />            
+    //         <CategoryFurniture />
+    //         {/* <ServiceOne /> */}            
+    //         {/* <ServiceTwo /> */}
+    //         <PosterTwo column="mb--10"/>
+    //         <Section>
+    //             <SectionTitle
+    //             title="Ən çox satılanlar"
+    //             subtitle=""
+    //             subtitleIcon=""
+    //             subColor="highlighter-primary"
+    //             />
+    //             <SlickSlider
+    //             class="explore-product-activation slick-layout-wrapper slick-layout-wrapper--15 axil-slick-arrow arrow-top-slide"
+    //             slidesToShow={1}
+    //             >
+    //                  {exploreProduct.slice(0, 2).map((product, index) => (
+    //                     <div key={index}>
+    //                         <div className="row row--15">
+    //                         {product.map((data) => (
+    //                             <div className="col-xl-3 col-lg-4 col-sm-6 col-12 mb--30" key={data.id}>
+    //                                 <ProductOne product={data} />
+    //                             </div>
+    //                         ))}
+    //                     </div>
+    //                     </div>
+    //                 ))}
+    //             </SlickSlider>        
+    //         </Section>
+            
+            
+    //         <Section>
+    //             <SectionTitle
+    //             title="Endirimdə olanlar"
+    //             subtitle=""
+    //             subtitleIcon=""
+    //             subColor="highlighter-primary"
+    //             />
+    //             <SlickSlider
+    //             class="explore-product-activation slick-layout-wrapper slick-layout-wrapper--15 axil-slick-arrow arrow-top-slide"
+    //             slidesToShow={1}
+    //             >
+    //                  {exploreProduct.slice(0, 2).map((product, index) => (
+    //                     <div key={index}>
+    //                         <div className="row row--15">
+    //                         {product.map((data) => (
+    //                             <div className="col-xl-3 col-lg-4 col-sm-6 col-12 mb--30" key={data.id}>
+    //                                 <ProductOne product={data} />
+    //                             </div>
+    //                         ))}
+    //                     </div>
+    //                     </div>
+    //                 ))}
+    //             </SlickSlider>        
+    //         </Section>           
+            
+            
+    //     </main>
+    //     <FooterTwo />
+    //     </>
+    // );
 }
  
 export default HomeFashion;
