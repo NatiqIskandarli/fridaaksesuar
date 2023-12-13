8afadebdea4e3bfe15dc30e888712b0ccbeb0f38
NatiqIskandarli <natiq.elect@gmail.com>
2023-12-14 00:20:25 +0400

    sade qeydiyyat fix

--- src/app/qeydiyyat/page.js
+++ src/app/qeydiyyat/page.js
@@ -77,6 +77,7 @@ const SignUp = () => {
                 try {
                     const boshRegister = await qeydiyyatKec(fullData)
                     localStorage.setItem("userid", boshRegister.user.id)
+                    alert(boshRegister.message)
                     router.push('/sign-in');
                 } catch (error) {
                     console.log(error)
